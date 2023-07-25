c5733acd639de3916eb7e4b91801204361bc7c74
Carlos J. Daboin <cdaboin2@gmail.com>
2023-07-25 11:30:36 -0300

    1) code in clearer sections 2) state functionality 3) chart title functinality 4) remove stringr package. 5) removed scales package. 6) api key in private script.

--- 2023_bls_api_etl.R
+++ 2023_bls_api_etl.R
@@ -19,8 +19,9 @@
 
 # TO DO:
 ## BASIC REVISION AND CLEANING: DONE
-## DO IT FOR STATES (BETTER!)
-## CREATE GITHUB PROJECT AND SET UP GITHUB ACTION (https://rfortherestofus.com/2023/05/github-actions/)
+## DO IT FOR STATES: DONE
+## CREATE GITHUB PROJECT: DONE 
+## SET UP GITHUB ACTION (https://rfortherestofus.com/2023/05/github-actions/)
 ### HANDLE PACKAGES
 ### THINK ABOUT HOW TO HANDLE API KEYS IN GITHUB 
 ## STREAMLINE CODE SO 
@@ -37,15 +38,14 @@ library(readr)
 # library(devtools)
 # devtools::install_package("keberwein/blscrapeR")
 library(blscrapeR)
-library(stringr)
 library(ggplot2)
-library(scales)
 options(scipen=999)# to see large numbers without scientific notation
 
 # First time, set up your BLS key to connect the API
-## set_bls_key("e75d562cec584b7bb3b847a50de75d5f", overwrite = TRUE)
-## 35655cc2988a448a87837cf13ec57533
-set_bls_key("34dfd2ee0cca4336bb63d89799808309", overwrite = TRUE)
+## I'm not sharing my key. Neither you should. It's stored in a sepparate script where
+## with line of code like this: set_bls_key("ccccxxxcccss", overwrite = TRUE)
+source("set_bls_key.R")
+
 # First time, reload your enviornment so you can use the key without restarting R.
 ## readRenviron("~/.Renviron")
 
@@ -63,66 +63,46 @@ counties<-blscrapeR::area_titles %>%
 city<-blscrapeR::area_titles %>% 
   filter(str_detect(area_fips,"CS"))
 
-us_total_states<-blscrapeR::area_titles %>% 
-  filter(str_detect(area_title,"U.S. TOTAL") |str_detect(area_title,"-- Statewide"))
 
 # County composition of MSAs - Merge this with county level estimates
 ## It is important because the API procesess FIPS+MSA+COUNTY CODES
 county_msa<-read_delim("raw/Crosswalk/regions/Census CBP/cbp_msa_county_reference12.txt", delim=",") %>% 
-  mutate(county=paste0(fipstate,fipscty))
+  mutate(county=paste0(fipstate,fipscty)) %>% 
+  mutate(state_title=base::gsub(pattern="(.*?), ",replacement="",x=name_county))
 
-# NAICS API CODES
-## General sector
+# 1.0 Define data Series Codes ------------------------------------
+
+##1.1 National CES by SECTOR---------------------------------------
+
+### General sector
 naics_1_codes_api <- read_csv("raw/naics_codes_api.csv") %>%
   filter(display_level==1) %>%
   mutate(code=paste0("CEU",industry_code,"001"))
 
-## By broad sector
+### Broad sector
 naics_2_codes_api <- read_csv("raw/naics_codes_api.csv") %>% 
   filter(display_level==2) %>% 
   mutate(code=paste0("CEU",industry_code,"01")) 
 
-#1.1 National CES by SECTOR---------------------------------------
+##1.2 State-wise CES by SECTOR---------------------------------------
 
-df_1 <- bls_api(naics_1_codes_api$code,
-                startyear = 2018, endyear = 2023, Sys.getenv("BLS_KEY"),catalog = TRUE) %>%
-  # Add time-series dates
-  dateCast()
+all_states<- unique(county_msa$state_title)
+selected_state_name<-"Florida"
 
-df_1_clean<-df_1 %>%
-  left_join(select(naics_1_codes_api,code,naics_code,industry_name,display_level),by=c("seriesID"='code'))
+# search the state name in the official state fips list
+selected_state<-county_msa %>% 
+  filter(state_title==selected_state_name) %>% 
+  pull(fipstate) %>% 
+  paste0("00000")
 
-## SAFETY CHECK: Data is squared
-df_1_clean %>% 
-  group_by(seriesID, industry_name,naics_code) %>% 
-  count()
+series_state_1<-paste("SMU",
+                      selected_state,
+                      str_remove(naics_1_codes_api$code,"CEU"),sep ="" )
 
-run_all<-FALSE
-
-if(run_all==TRUE){
-df_2 <- bls_api(naics_2_codes_api$code,
-                startyear = 2018, endyear = 2023, Sys.getenv("BLS_KEY"),catalog = TRUE) %>%
-  # Add time-series dates
-  dateCast()
-
-df_2_clean<-df_2 %>% 
-  left_join(select(naics_2_codes_api,code,naics_code,industry_name,display_level),by=c("seriesID"='code'))
-
-
-## SAFETY CHECK: Data is squared
-df_2_clean %>% 
-  group_by(seriesID, industry_name,naics_code) %>% 
-  count()
-}
-
-
-
-# 1.2 City-wise CES by SECTOR------------------------------------------------------
-
-
+##1.3 City-wise CES by SECTOR---------------------------------------
 all_cities<-unique(county_msa$name_msa)
 selected_city_name<-all_cities[all_cities=="Miami-Fort Lauderdale-West Palm Beach, FL Metro Area"] # Other cities we've worked with:# Kansas City, MO-KS 
-                           
+
 
 # search the city name in the official county-msa-state dataset
 selected_city<-county_msa %>% 
@@ -152,6 +132,43 @@ if (str_detect(selected_city_name,"New York")){
 series_city_1<-paste("SMU",
                      selected_city,
                      str_remove(naics_1_codes_api$code,"CEU"),sep ="" )
+
+# 2.0 Run ETL --------------------------------------------------------
+
+##2.1 National CES by SECTOR---------------------------------------
+
+df_1 <- bls_api(naics_1_codes_api$code,
+                startyear = 2018, endyear = 2023, Sys.getenv("BLS_KEY"),catalog = TRUE) %>%
+  # Add time-series dates
+  dateCast()
+
+df_1_clean<-df_1 %>%
+  left_join(select(naics_1_codes_api,code,naics_code,industry_name,display_level),by=c("seriesID"='code'))
+
+## SAFETY CHECK: Data is squared
+df_1_clean %>% 
+  group_by(seriesID, industry_name,naics_code) %>% 
+  count()
+
+
+## 2.2 State-wise CES by SECTOR------------------------------------------------------
+
+
+###This is how you build a state-sector code for the API
+df_state_1 <- bls_api(series_state_1,
+                       startyear = 2018, endyear = 2023, Sys.getenv("BLS_KEY"),catalog = TRUE) %>%
+  # Add time-series dates
+  dateCast()
+
+df_state_1_clean<-df_state_1 %>%
+  mutate(code=as.numeric(substr(seriesID,str_length(seriesID)-9,str_length(seriesID)-3))) %>%
+  left_join(naics_1_codes_api,by=c("code"="industry_code")) %>% 
+  mutate(value=value*1000)
+
+
+## 2.3 City-wise CES by SECTOR------------------------------------------------------
+
+
 df_cities_1 <- bls_api(series_city_1,
                        startyear = 2018, endyear = 2023, Sys.getenv("BLS_KEY"),catalog = TRUE) %>%
   # Add time-series dates
@@ -162,21 +179,56 @@ df_cities_1_clean<-df_cities_1 %>%
   left_join(naics_1_codes_api,by=c("code"="industry_code")) %>% 
   mutate(value=value*1000)
 
+# 2.4 RUN ETLS at two digits level-------------------
+run_all<-FALSE
+
 if(run_all==TRUE){
-series_city_2<-paste("SMU",
-                     selected_city,
-                     str_remove(naics_2_codes_api$code,"CEU"),sep ="" )
-
-df_cities_2 <- bls_api(series_city_2,
-                       startyear = 2018, endyear = 2023, Sys.getenv("BLS_KEY"),catalog = TRUE) %>%
-  # Add time-series dates
-  dateCast()
-
-df_cities_2_clean<-df_cities_2 %>%
-  mutate(code=as.numeric(substr(seriesID,str_length(seriesID)-9,str_length(seriesID)-2))) %>%
-  left_join(naics_2_codes_api,by=c("code"="industry_code")) %>% 
-  mutate(short_name=industry_name,
-         value=value*1000)
+  ## National level 2 digits-------------------------------------------------------------------
+  df_2 <- bls_api(naics_2_codes_api$code,
+                  startyear = 2018, endyear = 2023, Sys.getenv("BLS_KEY"),catalog = TRUE) %>%
+    # Add time-series dates
+    dateCast()
+  
+  df_2_clean<-df_2 %>% 
+    left_join(select(naics_2_codes_api,code,naics_code,industry_name,display_level),by=c("seriesID"='code'))
+  
+  
+  ### SAFETY CHECK: Data is squared
+  df_2_clean %>% 
+    group_by(seriesID, industry_name,naics_code) %>% 
+    count()
+  
+  ## State level 2 digits-------------------------------------------------------------------
+  series_state_2<-paste("SMU",
+                        selected_state,
+                        str_remove(naics_2_codes_api$code,"CEU"),sep ="" )
+  
+  df_state_2 <- bls_api(series_state_2,
+                        startyear = 2018, endyear = 2023, Sys.getenv("BLS_KEY"),catalog = TRUE) %>%
+    # Add time-series dates
+    dateCast()
+  
+  df_state_2_clean<-df_state_2 %>%
+    mutate(code=as.numeric(substr(seriesID,str_length(seriesID)-9,str_length(seriesID)-2))) %>%
+    left_join(naics_2_codes_api,by=c("code"="industry_code")) %>% 
+    mutate(short_name=industry_name,
+           value=value*1000)
+  
+  ## City level 2 digits-------------------------------------------------------------------
+  series_city_2<-paste("SMU",
+                       selected_city,
+                       str_remove(naics_2_codes_api$code,"CEU"),sep ="" )
+  
+  df_cities_2 <- bls_api(series_city_2,
+                         startyear = 2018, endyear = 2023, Sys.getenv("BLS_KEY"),catalog = TRUE) %>%
+    # Add time-series dates
+    dateCast()
+  
+  df_cities_2_clean<-df_cities_2 %>%
+    mutate(code=as.numeric(substr(seriesID,str_length(seriesID)-9,str_length(seriesID)-2))) %>%
+    left_join(naics_2_codes_api,by=c("code"="industry_code")) %>% 
+    mutate(short_name=industry_name,
+           value=value*1000)
 }
 
 #3.1  Series identified manually -----------------------------------------
@@ -196,7 +248,7 @@ set_1_no_yellow<-c("#E41A1C", "#377EB8", "#4DAF4A", "#984EA3", "#FF7F00", "black
 
 
 # Vector to order legend from highest to lowest employment index to date
-ces_plots<-function(data=df_1_clean, start_date="2019-12-01"){
+ces_plots<-function(data=df_1_clean, start_date="2019-12-01", title_text=""){
 
   
   order_legend<-data %>% 
@@ -256,10 +308,10 @@ ces_plots<-function(data=df_1_clean, start_date="2019-12-01"){
           legend.title.align = 0.3,
           legend.title = element_text(size=15), 
           legend.key.size = unit(1.4, "cm"))+
-    scale_y_continuous(labels=percent_format())+
+    scale_y_continuous()+
     scale_x_date(date_breaks = "1 months",date_labels = "%b %y")+
     scale_color_manual(values=set_1_no_yellow)+
-    labs(title = "Recent evolution of Nofarm employment",
+    labs(title = paste("Recent evolution of Nofarm employment", title_text),
          color="Sector",
          y="Jobs Index (Dec 2019 = 100)",
          x="Date",
@@ -284,7 +336,7 @@ ces_plots<-function(data=df_1_clean, start_date="2019-12-01"){
           legend.title.align = 0.3,
           legend.title = element_text(size=15), 
           legend.key.size = unit(1.4, "cm"))+
-    scale_y_continuous(labels=percent_format())+
+    scale_y_continuous()+
     scale_x_date(date_breaks = "1 months",date_labels = "%b %y")+
     scale_fill_manual(values=set_1_no_yellow)+
     theme(legend.position = "none")+
@@ -302,9 +354,15 @@ ces_plots<-function(data=df_1_clean, start_date="2019-12-01"){
   return(list(plot_0,plot_1,complement))
 }
 
-plots_cities_1<-ces_plots(data=df_cities_1_clean)
+
+plots_1<-ces_plots(data=df_1_clean, title_text = "- U.S Total")
+plots_1[[2]]
+
+
+plots_state_1<-ces_plots(data=df_state_1_clean, title_text = "- Florida")
+plots_state_1[[2]]
+
+
+plots_cities_1<-ces_plots(data=df_cities_1_clean, title_text = "- Miami")
 
 plots_cities_1[[2]]
-
-plots_1<-ces_plots(data=df_1_clean)
-plots_1[[2]]
