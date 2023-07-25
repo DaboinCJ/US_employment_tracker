73fccd3cce85637d7b37a46c7c6c2001fd538edc
Carlos J. Daboin <cdaboin2@gmail.com>
2023-07-25 11:36:09 -0300

    first batch of plots

--- 2023_bls_api_etl.R
+++ 2023_bls_api_etl.R
@@ -357,12 +357,17 @@ ces_plots<-function(data=df_1_clean, start_date="2019-12-01", title_text=""){
 
 plots_1<-ces_plots(data=df_1_clean, title_text = "- U.S Total")
 plots_1[[2]]
-
+ggsave(paste0("plots/national_chart_1_",
+              substr(Sys.Date(),0,7),".png"))
 
 plots_state_1<-ces_plots(data=df_state_1_clean, title_text = "- Florida")
 plots_state_1[[2]]
-
+ggsave(paste0("plots/state_florida_chart_1_",
+              substr(Sys.Date(),0,7),".png"))
 
 plots_cities_1<-ces_plots(data=df_cities_1_clean, title_text = "- Miami")
-
 plots_cities_1[[2]]
+ggsave(paste0("plots/city_miami_chart_1_",
+              substr(Sys.Date(),0,7),".png"))
+
+
